87f9d7d54f5f38573409b21da78c450361d1a93d
lexich <lexich121@gmail.com>
2012-08-21 22:58:32 +0400

    add media urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+
 admin.autodiscover()
+
+DEBUG=getattr(settings,"DEBUG",false)
+MEDIA_URL=getattr(settings,'MEDIA_URL','/media/')
+MEDIA_ROOT=getattr(settings,'MEDIA_ROOT','')
 
 urlpatterns = patterns('',
     url(r'^', include('index.urls', namespace='index')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if DEBUG:
+	urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
