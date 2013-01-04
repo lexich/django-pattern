681c78a6250bb0f9b6ec85136f4af26a0616bb74
lexich <lexich121@gmail.com>
2013-01-04 23:34:48 +0400

    implent grunt & bower tools

---
+++ grunt.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+  less: {
+    style: {
+      src: 'index/static/less/style.less',
+      dest: 'index/static/css/style.css',
+      options: {
+        compress: true
+      }
+    }
+  },  
+  copy:{
+    dist:{
+      files:{        
+        'index/static/js/lib/':[
+          'components/jquery/jquery.min.js',
+          'components/backbone/backbone-min.js',
+          'components/underscore/underscore-min.js',
+          'components/requirejs/require.js',          
+          'components/bootstrap/docs/assets/js/bootstrap.min.js',
+          'components/html5shiv/dist/*.js'
+        ],
+        'index/static/css/':[
+          'components/normalize-css/normalize.css',
+        ],
+        'index/static/img/':[
+          'components/bootstrap/img/*'
+        ]
+      },
+      options:{
+        flatten :true
+      }      
+    }    
+  },  
+});
+
+grunt.loadNpmTasks('grunt-less');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-bower');
+};
